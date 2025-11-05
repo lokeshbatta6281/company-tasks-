95a7f4098aa9e6ba96ac1e0f93d21ceb4b12c722
lokeshbatta6281 <lokeshbatta8@gmail.com>
2025-11-05 18:40:32 +0530

    completed day22 tasks

---
+++ day22/day22.js
@@ -0,0 +1,57 @@
+// Variables
+
+let name = "lokesh";
+const PI = 3.14159;
+var age = 23;
+
+let city; // Declaration
+
+
+// Data Types
+let text = "hi lokesh"; // String
+let num = 15; // Number
+let isActive = true; // Boolean ture/ false
+let a; // Undefined
+let person = null; // Null
+let color = ["red", "green", "blue"]; // Array
+let colors = ["red", "green", "blue", "Yellow"]; // Array
+let car = { 
+    make: "Toyota", 
+    model: "fortuner", 
+    year: 2024 
+}; 
+// Number
+let length = 20;
+let weight = 9.3;
+
+// BigInt
+let b = 1234567890123456789012345n;
+let c = BigInt(1234567890123456789012345)
+
+// Boolean
+let x2 = true;
+let y2 = false;
+
+
+
+// Undefined
+let f;
+let e;
+
+// Null
+let x1 = null;
+let y1 = null;
+
+
+document.getElementById("data").innerHTML = color;
+document.getElementById("data").innerHTML = colors;
+document.getElementById("data").innerHTML = text;
+document.getElementById("data").innerHTML = num;
+document.getElementById("data").innerHTML =
+document.getElementById("data").innerHTML = color; 
+
+console.log(text);
+console.log(color);
+console.log(length);
+console.log(num);
+console.log(weight);
