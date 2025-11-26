852604a5e7b5fceb79eefc7ff42af0fd3719db4f
lokeshbatta6281 <lokeshbatta8@gmail.com>
2025-11-26 18:17:19 +0530

    completed team task

---
+++ team task2/teamtask.js
@@ -0,0 +1,19 @@
+function setmoodHappy() {
+    document.body.style.background = "yellow";
+    document.getElementById("text").innerText = "I am feeling Great! 😊";
+}
+
+function setmoodSad() {
+    document.body.style.background = "blue";
+    document.getElementById("texte").innerText = "I am feeling down today. 😢";
+}
+
+function setmoodAngry() {
+    document.body.style.background = "red";
+    document.getElementById("text").innerText = "I am frustrated! 😡";
+}
+
+function setmoodSleepy() {
+    document.body.style.background = "purple";
+    document.getElementById("text").innerText = "I need a nap. 😴";
+}
